17e96fe04bef2500e9e31b7f01458f84220d6952
Frances Fisk <francesdfisk@gmail.com>
2018-07-25 13:02:56 -0700

    added travis and static.json file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
+// This file is run automatically before any tests. It sets up an adaptor, which lets Enzyme know how to work with components made with a specific version of React.
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
